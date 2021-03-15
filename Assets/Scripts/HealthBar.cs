5aab9bcce4b2a2d220c90ca08a15997145ac4d66
Simon Hughes <simonhughes22@hotmail.com>
2021-03-15 16:51:12 -0500

    Add scenes

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Image[] hearts;
+    [SerializeField] Image heartImage;      
+    [SerializeField] float heartWidth = 2f;
+
+    [SerializeField] float anchorX;
+    [SerializeField] float anchorY;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        hearts = new Image[GameState.PlayerMaxHealth];
+        for (int i = 0; i < GameState.PlayerMaxHealth; i++)
+        {
+            Image heart = Instantiate(heartImage, this.gameObject.transform, false);            
+            heart.transform.position = new Vector3(anchorX + (heartWidth * i),
+                anchorY, transform.position.z);
+            hearts[i] = heart;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < GameState.PlayerMaxHealth; i++)
+        {
+            Image heart = hearts[i];
+            if (i < GameState.Instance.PlayerHealth)
+            {
+                heart.enabled = true;
+            }
+            else
+            {
+                heart.enabled = false;
+            }            
+        }
+    }
+}
